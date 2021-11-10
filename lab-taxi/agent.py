44dd368c956076f4273563a466b262e83b287064
p.bazan <p.bazan@samsung.com>
2021-11-10 09:09:37 +0100

    Adding my excercises

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -1,40 +1,69 @@
 import numpy as np
 from collections import defaultdict
 
-class Agent:
 
-    def __init__(self, nA=6):
-        """ Initialize agent.
+def greedy_action(Q, state):
+    return np.argmax(Q[state])
 
-        Params
-        ======
-        - nA: number of actions available to the agent
-        """
+
+def epsilon_greedy_action(Q, state, eps, nA):
+    if np.random.random() > eps:
+        return greedy_action(Q, state)
+    else:
+        probs = np.ones(nA) * eps / nA
+        best_a = np.argmax(Q[state])
+        probs[best_a] = 1 - eps + eps / nA
+        return np.random.choice(np.arange(nA), p=probs)
+
+
+def random_action(nA):
+    return np.random.choice(np.arange(nA))
+
+
+
+
+class SarsaAgent:
+
+    def __init__(self, nA, alpha=.1, gamma=1.):
         self.nA = nA
+        self.alpha = alpha
+        self.gamma = gamma
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.eps = 1.
+        self.next_action = None
+        self.iter = 0
 
     def select_action(self, state):
-        """ Given the state, select an action.
+        if self.next_action:
+            return self.next_action
+        else:
+            return epsilon_greedy_action(self.Q, state, self.eps, self.nA)
 
-        Params
-        ======
-        - state: the current state of the environment
+    def calc_target(self, new_state):
+        new_action = epsilon_greedy_action(self.Q, new_state, self.eps, self.nA)
+        self.next_action = new_action
+        return self.Q[new_state][new_action]
 
-        Returns
-        =======
-        - action: an integer, compatible with the task's action space
-        """
-        return np.random.choice(self.nA)
+    def step(self, state, action, reward, new_state, done):
+        self.iter += 1
+        self.eps = max(1. / self.iter, .05)
+        target = reward + self.gamma * self.calc_target(new_state)
+        self.Q[state][action] *= (1 - self.alpha)
+        self.Q[state][action] += self.alpha * target
 
-    def step(self, state, action, reward, next_state, done):
-        """ Update the agent's knowledge, using the most recently sampled tuple.
 
-        Params
-        ======
-        - state: the previous state of the environment
-        - action: the agent's previous choice of action
-        - reward: last reward received
-        - next_state: the current state of the environment
-        - done: whether the episode is complete (True or False)
-        """
-        self.Q[state][action] += 1
+class SarsaMax(SarsaAgent):
+
+    def calc_target(self, new_state):
+        """ Calculates best value in new state """
+        return np.max(self.Q[new_state])
+
+
+class SarsaExpected(SarsaMax):
+
+    def calc_target(self, new_state):
+        """ Calculates expected value in new state """
+        probs = np.ones(self.nA) * self.eps / self.nA
+        best_a = np.argmax(self.Q[new_state])
+        probs[best_a] = 1 - self.eps + self.eps / self.nA
+        return np.dot(self.Q[new_state], probs)
--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -1,8 +1,8 @@
-from agent import Agent
+from agent import *
 from monitor import interact
 import gym
 import numpy as np
 
 env = gym.make('Taxi-v2')
-agent = Agent()
+agent = Sarsa()
 avg_rewards, best_avg_reward = interact(env, agent)
