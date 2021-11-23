647bd06ca52c8b81cebab6924af35fac204f37f0
p.bazan <p.bazan@samsung.com>
2021-11-23 15:53:51 +0100

    Adding strategies

---
+++ p1_navigation/strategy.py
@@ -0,0 +1,95 @@
+from abc import ABC, abstractmethod
+from torch import nn
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class BaseStrategy(ABC):
+
+    def initialize(self):
+        pass
+
+    @abstractmethod
+    def select_action(self, model: nn.Module, state):
+        pass
+
+    def update(self):
+        pass
+
+    @abstractmethod
+    def display(self, episodes: int):
+        pass
+
+
+class GreedyStrategy(BaseStrategy):
+
+    def select_action(self, model: nn.Module, state):
+        with torch.no_grad():
+            q_values = model(state).cpu().detach().data.numpy()
+        return np.argmax(q_values)
+
+    def display(self, episodes: int):
+        print('Nothing to dislpay. Strategy is greedy.')
+
+
+class EpsilonGreedyStrategy(BaseStrategy):
+
+    def __init__(self, eps_start):
+        self.eps_start = eps_start
+        self.epsilon = eps_start
+
+    def initialize(self):
+        self.epsilon = self.eps_start
+
+    def select_action(self, model: nn.Module, state):
+        with torch.no_grad():
+            q_values = model(state).cpu().detach().data.numpy()
+
+        if np.random.rand() > self.epsilon:
+            return np.argmax(q_values)
+        else:
+            return np.random.choice(len(q_values))
+
+    def update(self):
+        pass  # nothing to do here - epsilon is const
+
+    def display(self, episodes: int, ax=None):
+        self.initialize()
+        epsilons = np.empty(episodes, dtype='float')
+        for i in range(episodes):
+            epsilons[i] = self.epsilon
+            self.update()
+
+        if not ax:
+            fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+        ax.plot(epsilons)
+        ax.set_title(self.__class__.__name__)
+        plt.grid(True)
+        plt.show()
+
+
+class LinearEpsilonGreedyStrategy(EpsilonGreedyStrategy):
+
+    def __init__(self, eps_start=1., eps_min=.1, decay=.005):
+        super().__init__(eps_start)
+        self.eps_start = eps_start
+        self.eps_min = eps_min
+        self.decay = decay
+
+    def update(self):
+        self.epsilon -= self.decay
+        self.epsilon = max(self.epsilon, self.eps_min)
+
+
+class ExponentialEpsilonGreedyStrategy(EpsilonGreedyStrategy):
+
+    def __init__(self, eps_start=1., eps_min=.1, decay=.005):
+        super().__init__(eps_start)
+        self.eps_start = eps_start
+        self.eps_min = eps_min
+        self.decay = decay
+
+    def update(self):
+        self.epsilon *= self.decay
+        self.epsilon = max(self.epsilon, self.eps_min)
---
+++ p1_navigation/test_strategy.py
@@ -0,0 +1,34 @@
+from strategy import EpsilonGreedyStrategy, LinearEpsilonGreedyStrategy, ExponentialEpsilonGreedyStrategy
+from torch import nn
+import torch
+
+
+def test_epsilon_greedy_strategy():
+    strat = EpsilonGreedyStrategy(.05)
+    model = nn.Linear(10, 2)
+    state = torch.randn(10)
+    action = strat.select_action(model, state)
+    assert 0 <= action <= 1
+
+    strat.display(1000) # uncomment to see chart
+
+
+def test_linear_epsilon_greedy_strategy():
+    strat = LinearEpsilonGreedyStrategy(1, .1, .005)
+    model = nn.Linear(10, 2)
+    state = torch.randn(10)
+    action = strat.select_action(model, state)
+    assert 0 <= action <= 1
+
+    strat.display(1000) # uncomment to see chart
+
+
+def test_exponential_epsilon_greedy_strategy():
+    strat = ExponentialEpsilonGreedyStrategy(1, .1, .995)
+    model = nn.Linear(10, 2)
+    state = torch.randn(10)
+    action = strat.select_action(model, state)
+    assert 0 <= action <= 1
+
+    strat.display(1000) # uncomment to see chart
+
